9910bb5c3c4acd08323de2b8794aaabd8dc57c5a
k-sarito <coryclark95@gmail.com>
2022-06-21 13:41:39 -0500

    DogRepository

--- DogGo/Models/Dog.cs
+++ DogGo/Models/Dog.cs
@@ -2,5 +2,10 @@
 {
     public class Dog
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Breed { get; set; }
+        public string Notes { get; set; }
+        public string ImageURL { get; set; }
     }
 }
